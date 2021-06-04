b708fcd2ea49487ccfc9fca3793cfe3f00308db9
MadhaiGIT <madhai.info@gmail.com>
2021-06-04 19:07:25 -0400

    fix for facebook redirect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     @yield('styles')
 </head>
-<body data-smooth-scroll-offset="77">
+<body data-smooth-scroll-offset="77" id="_=_">
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="8afdmI4G"></script>
 
