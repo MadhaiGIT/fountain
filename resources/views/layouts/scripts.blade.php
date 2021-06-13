ff59ff44c9b7e6ab7b812f573e4a7f655215c9d2
MadhaiGIT <madhai.info@gmail.com>
2021-06-13 18:56:54 -0400

    google tag manager added

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -4,3 +4,15 @@
 <script src="{{asset('js/countdown.min.js')}}"></script>
 <script src="{{asset('js/smooth-scroll.min.js')}}"></script>
 <script src="{{asset('js/scripts.js')}}"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N9GBGRBR89"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-N9GBGRBR89');
+</script>
